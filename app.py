bc25e7e809b84b970ead22ea7e94ec078dd5f9bf
ravage400 <jasonarana456@gmail.com>
2025-02-14 07:01:23 -0800

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,12 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "Sentinel-Intel is Running!"
+    return render_template("index.html")
 
 if __name__ == "__main__":
-    app.run()
+    import os
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
